44de0c9045bfc269789666506c9d9739e30f7ab9
Adran Carnavale <adran.carnavale@gmail.com>
2026-03-15 11:07:00 -0300

    refact: project organization

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-#!/usr/bin/env npx tsx
-
 import * as cdk from "aws-cdk-lib";
 
-import { CorrectStack } from "../src/lib/correct-stack.js";
-import { WrongStack } from "../src/lib/wrong-stack.js";
+import { CorrectStack } from "@/lib/correct-stack.js";
+import { WrongStack } from "@/lib/wrong-stack.js";
 
 const app = new cdk.App();
 
